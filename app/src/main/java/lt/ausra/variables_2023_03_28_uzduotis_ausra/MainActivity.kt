827740362587ba82e483a1961b236a0e2399137a
apaskauskyte <a.paskauskyte@gmail.com>
2023-03-29 17:04:15 +0300

    Initial commit

---
+++ app/src/main/java/lt/ausra/variables_2023_03_28_uzduotis_ausra/MainActivity.kt
@@ -0,0 +1,11 @@
+package lt.ausra.variables_2023_03_28_uzduotis_ausra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
