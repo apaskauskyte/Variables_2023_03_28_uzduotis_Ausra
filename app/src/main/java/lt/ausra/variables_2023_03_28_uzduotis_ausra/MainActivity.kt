7b589119f2f3c9ffbe64c79f3a0ad93667306ee8
apaskauskyte <a.paskauskyte@gmail.com>
2023-03-29 18:26:14 +0300

    Add variables

--- app/src/main/java/lt/ausra/variables_2023_03_28_uzduotis_ausra/MainActivity.kt
+++ app/src/main/java/lt/ausra/variables_2023_03_28_uzduotis_ausra/MainActivity.kt
@@ -2,10 +2,27 @@ package lt.ausra.variables_2023_03_28_uzduotis_ausra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "my_tag"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var myName = "Ausra"
+        val vardoRaidziuSkaicius = myName.length
+        val kuroKaina = 1.49F
+        val rezultatas = vardoRaidziuSkaicius + kuroKaina
+        val arRezultatasTeigiamas = rezultatas > 0
+        val vardoPirmojiRaide = 'a'
+        val pirmosRaidesId = vardoPirmojiRaide.code
+        val rezultatas02 = (
+                vardoRaidziuSkaicius - pirmosRaidesId
+                ) + rezultatas
+        val arRezultatas02Teigiamas = rezultatas02 > 0
+
     }
 }
+
